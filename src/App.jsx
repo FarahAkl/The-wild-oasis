0df7abdafd7ada27a42968b0bd38c38d6f31000f
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-09-19 21:30:36 +0300

    feat: restructure App component to implement routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,63 @@
-import styled from "styled-components";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import DashBoard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
 
-const StyledDiv = styled.div`
-  padding: 20px;
-`;
-
-function App() {
+export default function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledDiv>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Hello World!</Heading>
-            <div>
-              <Heading as="h2">Hello World!</Heading>
-              <Heading as="h3">Hello World!</Heading>
-              <Button
-                variation="primary"
-                size="small"
-              >
-                Farah
-              </Button>
-
-              <Button
-                variation="secondary"
-                size="medium"
-              >
-                Farah
-              </Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as="h3">form</Heading>
-            <form>
-              <Input type="number" />
-              <Input type="number" />
-            </form>
-          </Row>
-        </Row>
-      </StyledDiv>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            index
+            element={
+              <Navigate
+                replace
+                to="dashboard"
+              />
+            }
+          />
+          <Route
+            path="dashboard"
+            element={<DashBoard />}
+          />
+          <Route
+            path="account"
+            element={<Account />}
+          />
+          <Route
+            path="bookings"
+            element={<Bookings />}
+          />
+          <Route
+            path="cabins"
+            element={<Cabins />}
+          />
+          <Route
+            path="login"
+            element={<Login />}
+          />
+          <Route
+            path="settings"
+            element={<Settings />}
+          />
+          <Route
+            path="users"
+            element={<Users />}
+          />
+          <Route
+            path="*"
+            element={<PageNotFound />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
-export default App;
