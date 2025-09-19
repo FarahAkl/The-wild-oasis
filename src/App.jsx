10cb015b73c8493aa7f03b93c44b1c5042cb84f6
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-09-19 21:55:54 +0300

    feat: add AppLayout, Header, and Sidebar components for improved layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 
 export default function App() {
   return (
@@ -15,42 +16,44 @@ export default function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route
-            index
-            element={
-              <Navigate
-                replace
-                to="dashboard"
-              />
-            }
-          />
-          <Route
-            path="dashboard"
-            element={<DashBoard />}
-          />
-          <Route
-            path="account"
-            element={<Account />}
-          />
-          <Route
-            path="bookings"
-            element={<Bookings />}
-          />
-          <Route
-            path="cabins"
-            element={<Cabins />}
-          />
+          <Route element={<AppLayout />}>
+            <Route
+              index
+              element={
+                <Navigate
+                  replace
+                  to="dashboard"
+                />
+              }
+            />
+            <Route
+              path="dashboard"
+              element={<DashBoard />}
+            />
+            <Route
+              path="account"
+              element={<Account />}
+            />
+            <Route
+              path="bookings"
+              element={<Bookings />}
+            />
+            <Route
+              path="cabins"
+              element={<Cabins />}
+            />
+            <Route
+              path="settings"
+              element={<Settings />}
+            />
+            <Route
+              path="users"
+              element={<Users />}
+            />
+          </Route>
           <Route
             path="login"
             element={<Login />}
-          />
-          <Route
-            path="settings"
-            element={<Settings />}
-          />
-          <Route
-            path="users"
-            element={<Users />}
           />
           <Route
             path="*"
