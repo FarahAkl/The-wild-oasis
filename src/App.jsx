b2c1586c12126cbb6c2bcf8eed8a7f511eb3122e
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-09-25 14:04:13 +0300

    feat: add @tanstack/react-query and @tanstack/react-query-devtools for data fetching and state management

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,21 @@ import Users from "./pages/Users";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -61,6 +72,6 @@ export default function App() {
           />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
