097298cef7d139febffc81cad723d8ff52cfd95f
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-10-24 21:06:02 +0300

    feat: add sorting functionality to CabinTable; implement SortBy component and integrate it into CabinTableOperations

--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const StyledSelect = styled.select`
   font-size: 1.4rem;
@@ -13,3 +14,31 @@ const StyledSelect = styled.select`
   font-weight: 500;
   box-shadow: var(--shadow-sm);
 `;
+
+export default function Select( { options, value,onChange, ...props } ) {
+  return (
+    <StyledSelect value={value} onChange={onChange} {...props} >
+      {options.map((option) => {
+        return (
+          <option
+            key={option.value}
+            value={option.value}
+          >
+            {option.label}
+          </option>
+        );
+      })}
+    </StyledSelect>
+  );
+}
+
+Select.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+      label: PropTypes.node,
+    })
+  ),
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  onChange: PropTypes.func,
+};
