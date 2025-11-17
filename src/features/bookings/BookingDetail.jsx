d9d3f845e7d851926800188726c748cb4eed661f
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2025-11-17 18:17:37 +0200

    feat: implement checkout functionality in BookingDetail and BookingRow; add useCheckOut hook for managing checkout state

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,7 @@ import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./useBooking";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
+import { useCheckOut } from "../check-in-out/useCheckOut";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,6 +22,7 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const navigate = useNavigate();
+  const { checkout, isCheckingOut } = useCheckOut();
 
   const { booking, isPending } = useBooking();
   const moveBack = useMoveBack();
@@ -50,6 +52,14 @@ function BookingDetail() {
         {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
+          </Button>
+        )}
+        {status === "checked-in" && (
+          <Button
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingOut}
+          >
+            Check out
           </Button>
         )}
         <Button
